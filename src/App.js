84520e6fa39b4eb36edc6d833926e739c8582de6
leoaversa <aversaleonardo@hotmail.com>
2022-10-18 23:39:22 -0300

    commit 1

--- src/App.js
+++ src/App.js
@@ -1,23 +1,59 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from "react";
+import "./App.css";
+import Boton from "./Components/Boton/Boton";
+import BotonClear from "./Components/BotonClear/BotonClear";
+import Pantalla from "./Components/Pantalla/Pantalla";
 
 function App() {
+  const [input, setInput] = useState ( 'input');  
+
+  const agregarInput = val => {
+    setInput(val + input)
+  } ;
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="react-logo-contenedor">
+        <img
+          className="react-logo"
+          src="logoReact.png"
+          alt="logo de React para Calculadora"
+        />
+      </div>
+
+      <div className="contenedor-calculadora ">
+        <Pantalla input={input}/> 
+        <div className="fila">
+          <Boton>1</Boton>
+          <Boton>2</Boton>
+          <Boton>3</Boton>
+          <Boton>+ </Boton>
+        </div>
+
+        <div className="fila">
+        <Boton>4</Boton>
+          <Boton>5</Boton>
+          <Boton>6</Boton>
+          <Boton>- </Boton>
+          </div>
+
+        <div className="fila">
+        <Boton>7</Boton>
+          <Boton>8</Boton>
+          <Boton>9</Boton>
+          <Boton>*  </Boton>
+        </div>
+        <div className="fila">
+        <Boton>=</Boton>
+          <Boton>0</Boton>
+          <Boton>.</Boton>
+          <Boton>/</Boton>
+          </div>
+        <div className="fila">
+        <BotonClear>Clear</BotonClear>
+          
+          </div>
+      </div>
     </div>
   );
 }
---
+++ src/Components/Pantalla/Pantalla.js
@@ -0,0 +1,8 @@
+import React from "react";
+import "./Pantalla.css";
+
+const Pantalla = ({ input }) => {
+  return <div className="input"></div>;
+};
+
+export default Pantalla;
